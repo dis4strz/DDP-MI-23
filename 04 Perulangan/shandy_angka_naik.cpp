bdef134bb39ca8fff20f15356ee702ad64bc14ca
dis4strz <shandydewangga72@gmail.com>
2023-10-12 23:05:00 +0700

    tugas perulangan

---
+++ 04 Perulangan/shandy_angka_naik.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    if (N >= 1)
+    {
+        for (int i = 1; i <= N; i++)
+        {
+            cout << i << endl;
+        }
+    }
+    else if (N >= 1)
+    {
+    }
+    return 0;
+}
