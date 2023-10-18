289ccffbe5cdc69afaeca426b5167dbd3f7734bb
dis4strz <shandydewangga72@gmail.com>
2023-10-18 10:21:28 +0700

    tugas array

---
+++ 05 Array/shandy_satu_satu.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string str;
+    getline(cin, str);
+    for (int i = 0; i < str.size(); i++)
+    {
+        cout << str[i] << endl;
+    }
+    return 0;
+}
